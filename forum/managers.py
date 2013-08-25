ee2ff0b94f4b8ccaa57cc7cde7c7f0ce391305b9
encorehu <huyoo353@126.com>
2013-08-25 21:03:53 +0800

    modify managers.py, some naming.

--- forum/managers.py
+++ forum/managers.py
@@ -2,19 +2,22 @@ from django.db import models
 from django.db.models import Q
 
 class ForumManager(models.Manager):
-    def for_user(self, u):
-        if u and u.is_authenticated():
-            public = Q(allowed_users__isnull=True)
-            user = Q(allowed_users=u)
-            return self.filter(public|user).distinct()
+    def for_user(self, user):
+        if user and user.is_authenticated():
+            public_query = Q(allowed_users__isnull=True)
+            user_query   = Q(allowed_users=user)
+            return self.filter(public_query|user_query).distinct()
         return self.filter(allowed_users__isnull=True)
 
     def for_groups(self, groups):
         if groups:
-            public = Q(groups__isnull=True)
-            user_groups = Q(groups__in=groups)
-            return self.filter(public|user_groups).distinct()
+            public_query      = Q(groups__isnull=True)
+            user_groups_query = Q(groups__in=groups)
+            return self.filter(public_query|user_groups_query).distinct()
         return self.filter(groups__isnull=True)
     
     def has_access(self, forum, user):
         return forum in self.for_user(user)
+
+    def has_access2(self, forum, groups):
+        return forum in self.for_groups(groups)
